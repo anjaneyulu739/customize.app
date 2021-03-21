d18fca76c85f5d6b0d4f72f144caeb2fb925a8a4
sainath8897 <saimanchikatla123@gmail.com>
2021-03-21 13:14:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
 
+import { Component} from '@angular/core';
+import{CdkDragDrop,moveItemInArray }from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-dream-app';
+  todo = [
+    'Slider images',
+    'Image Grid',
+    'Banner images',
+    'Product Grid',
+    'Instagram grid',
+    
+  ];
+drop(event: CdkDragDrop<string[]>){
+  moveItemInArray(this.todo, event.previousIndex,event.currentIndex);
 }
+}
+  
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import{DragDropModule} from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IphoneComponent } from './iphone/iphone.component';
+import { DropdownbuttonComponent } from './dropdownbutton/dropdownbutton.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IphoneComponent,
+    DropdownbuttonComponent 
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
